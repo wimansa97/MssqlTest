06f1ee4601b8aa53eb2eb79a4ad105a32c223cee
AMILALAP <wimansa>
2019-11-08 13:35:40 +0530

    added ececute

--- execute.php
+++ execute.php
@@ -1,4 +1,31 @@
 <?php
-include './execute1.php';
-$connection = new execute();
-$conn = $connection->execute();
+include './connection.php';
+class execute
+{
+    var $con;
+    var $c;
+    function execute()
+    {
+        $this->c = new connection();
+        $this->con = $this->c->openConnection();
+
+        try {
+            $tsql = "SELECT UserName FROM dbo.Users";  
+            $getuser = sqlsrv_query($this->con, $tsql);  
+            if ($getuser == FALSE)  
+                die(FormatErrors(sqlsrv_errors()));  
+            $productCount = 0;  
+            while($row = sqlsrv_fetch_array($getuser, SQLSRV_FETCH_ASSOC))  
+            {  
+                echo($row['UserName']);  
+                echo("<br/>");  
+                $productCount++;  
+            }  
+            echo $productCount;
+            sqlsrv_free_stmt($getuser);  
+            sqlsrv_close($this->con);  
+         } catch (exception $e) {
+            echo ("Errors!");
+        }
+    }
+}
