c019fa0877eccaa2a15339953809dbd0407094d7
AMILALAP <wimansa>
2019-11-08 12:19:47 +0530

    MSSQL

---
+++ connection.php
@@ -0,0 +1,21 @@
+<?php
+class connection
+{
+    function openConnection()
+    {
+        try {
+
+            $serverName = "acomit.office-on-the.net";
+            $connectionOption = array("Database" => "dayan_org", "Uid" => "sa", "PWD" => "acom49339");
+
+            $conn = sqlsrv_connect($serverName, $connectionOption);
+
+            if ($conn == false)
+                die(FormatErrors(sqlsrv_error()));
+            else
+                echo "Connected..";
+        } catch (Exception $e) {
+            echo ("Error!");
+        }
+    }
+}
